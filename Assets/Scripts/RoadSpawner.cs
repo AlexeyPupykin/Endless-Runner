f1d71415e5acb66e64af3251ea701d4503dc4bd8
AlexeyPupykin <mymailvrn98@gmail.com>
2023-01-11 01:21:26 +0300

    player; road; road spawner;

---
+++ Assets/Scripts/RoadSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour
+{
+    public List<GameObject> roads;
+    private float offset = 56f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (roads != null && roads.Any())
+        {
+            roads = roads.OrderBy(r => r.transform.position.z).ToList();
+        }
+    }
+    
+    public void ModeRoad()
+    {
+        GameObject modedRoad = roads[0];
+        roads.Remove(modedRoad);
+        float newZ = roads[roads.Count - 1].transform.position.z + offset;
+        modedRoad.transform.position = new Vector3(0, 0, newZ);
+        roads.Add(modedRoad);
+    }
+}
