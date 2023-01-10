54a0c53314af503b4156386b331481a0d8d2e5b4
AlexeyPupykin <mymailvrn98@gmail.com>
2023-01-11 02:28:48 +0300

    created side content;

---
+++ Assets/Scripts/PlotSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotSpawner : MonoBehaviour
+{
+    private int initAmount = 5;
+    private float initStartZ = 25;
+    private float plotSize = 56f;
+    private float xPosLeft = -36f;
+    private float xPosRight = 36f;
+    private float lastZpos = -56f;
+
+    public List<GameObject> plots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < initAmount; i++)
+        {
+            SpawnPlot();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnPlot()
+    {
+        GameObject plotLeft = plots[Random.Range(0, plots.Count)];
+        GameObject plotRight = plots[Random.Range(0, plots.Count)];
+
+        float zPos = lastZpos + plotSize;
+
+        Instantiate(plotLeft, new Vector3(xPosLeft, 0.025f, zPos), plotLeft.transform.rotation);
+        Instantiate(plotRight, new Vector3(xPosRight, 0.025f, zPos), new Quaternion(0, 180, 0, 0));
+
+        lastZpos += plotSize;
+    }
+}
