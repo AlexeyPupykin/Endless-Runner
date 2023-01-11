0fcb27a7c7cb165ecc24d8e822900f86257074c3
AlexeyPupykin <mymailvrn98@gmail.com>
2023-01-12 00:36:32 +0300

    model + run + jump;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,13 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed = 10f;
     public float minXValue = -6.5f;
     public float maxXValue = 6.5f;
+    private float previousX;
+    public float rotationSpeed = 100f;
+    //private float previousY;
+
     public SpawnManager spawnManager;
+
+    public Animator model;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +24,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
-        float vMovement = Input.GetAxis("Vertical") * movementSpeed;
+        previousX = transform.position.x;
 
-        transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
+        //float vMovement = Input.GetAxis("Vertical") * movementSpeed;
+
+        transform.Translate(new Vector3(hMovement, 0, movementSpeed) * Time.deltaTime);
+
+        var deltaX = transform.position.x - previousX;
+
+
+        model.transform.eulerAngles = new Vector3(transform.eulerAngles.y, deltaX * rotationSpeed, transform.eulerAngles.z);
 
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, minXValue, maxXValue), transform.position.y, transform.position.z);
 
-        //transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(999);
+            model.Play("Jump");
+            GetComponent<Animator>().Play("Jump");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
